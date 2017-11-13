32fe09a735d5e3a15193e58acea3499632c279a5
Brennan Turner <bltsec.1337@gmail.com>
2017-11-13 11:27:23 -0600

    Circle CI

---
+++ src/test/java/ninja/bltsec/models/CategoryTest.java
@@ -0,0 +1,33 @@
+package ninja.bltsec.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4L;
+        category.setId(4L);
+        assertEquals(idValue, category.getId());
+    }
+
+}
